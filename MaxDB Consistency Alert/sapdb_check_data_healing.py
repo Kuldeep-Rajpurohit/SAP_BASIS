47103d7779d48a49b1905531eead549a97899ae6
Kuldeep Rajpurohit <51077392+Kuldeep-Rajpurohit@users.noreply.github.com>
2023-01-24 07:19:35 +0530

    Return codes correction
    Return codes added to healing script

--- MaxDB Consistency Alert/sapdb_check_data_healing.py
+++ MaxDB Consistency Alert/sapdb_check_data_healing.py
@@ -7,14 +7,14 @@ from socket import gethostbyaddr, gethostname
 
 
 def unix_cmd(cmd):
-        result = subprocess.check_output(cmd, shell=True)
-        return(result.decode())
+    result = subprocess.check_output(cmd, shell=True)
+    return(result.decode())
 
 
 def check_db_type():
-        cmd = "cat /etc/fstab | grep -i sapdata1 | awk '{print $2}'"
-        result = unix_cmd(cmd)
-        return("sapdb" in result)
+    cmd = "cat /etc/fstab | grep -i sapdata1 | awk '{print $2}'"
+    result = unix_cmd(cmd)
+    return("sapdb" in result)
 
 
 def sendMail(msg):
@@ -28,10 +28,10 @@ def sendMail(msg):
     try:
         smtpObj = smtplib.SMTP(smtpServer)
         smtpObj.sendmail(sender, notification_receivers, message)
-        print("Email Successfully sent")
+        # print("Email Successfully sent")
     except smtplib.SMTPException:
         print("Error: unable to send email")
-        return(2)
+        exit(2)
 
     
 def update_flagfile(val):
@@ -41,7 +41,6 @@ def update_flagfile(val):
     return
 
 def get_flagvalue():
-    
     file_name = "/var/log/nagios/heal_db_chkdata_flagfile"
     with open(file_name, 'r') as ffile:
         flagValue = ffile.read()
@@ -59,20 +58,22 @@ class MaxDB:
         try:
             cmd = "su - sqd%s -c \"x_cons %s show active\"| grep -i chkdata | wc -l"%(self.sid.lower(), self.sid)
             output = int(unix_cmd(cmd).strip("\n"))
-            return(output)
+            return(0, output)
         except:
             msg = "Error finding running processes from database."
             print(msg)
             sendMail(msg)
-            exit(2)
+            return(2)
 
 
     def trigger_checkdata(self):
         try:
             cmd = "su - sqd%s -c \"nohup dbmcli -U c db_execute check data &\""%self.sid.lower()
             subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-            if self.check_data_running():
-                print("Triggered check data.")
+            temp = self.check_data_running()[1]
+            if temp > 0:
+                # print("Triggered check data.")
+                return
             else:
                 msg = "Failed to trigger check data. Kindly trigger manually"
                 print(msg)
@@ -81,34 +82,38 @@ class MaxDB:
             msg = "Error triggering check data. Check manually."
             print(msg)
             sendMail(msg)
-            exit(2)
+            return(2)
 
+exit_code = 0
 
 def main():
     flag = get_flagvalue()
     if flag == '1':
         print("Self healing already in process. Quitting")
-        exit(0)
+        return(0)
     else:
         maxdb = MaxDB()
         maxdb.get_SID()
-        already_running = maxdb.check_data_running()
-        
-        if not already_running:
-            maxdb.trigger_checkdata()
-            print("Self healing triggered.")
-            update_flagfile('1')
-            
+        exit_code, already_running = maxdb.check_data_running()
+        if exit_code == 0:
+            if not already_running:
+                maxdb.trigger_checkdata()
+                print("Self healing triggered.")
+                update_flagfile('1')
+                
+            else:
+                print("Check data already running.")
+                update_flagfile('1')
         else:
-            print("Check data already running.")
-            update_flagfile('1')
-
+            return(exit_code)
 try: 
     if __name__ == '__main__':
-        main()
+        exit_code = main()
 
 except:
     msg = "Critical. Error checking the status"
     print(msg)
     sendMail(msg)
-    
+    exit_code = 2
+
+exit(exit_code)
